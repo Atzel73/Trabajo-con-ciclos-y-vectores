9d2b6a34c6b7cefeef2596d69f5bce17e83aa382
Atzel73 <axelandrade638mail.com>
2021-05-07 19:32:32 -0500

    Re trabajo con ciclos terminado.

--- ALumnos_Ciclos.java
+++ ALumnos_Ciclos.java
@@ -16,32 +16,37 @@ public class ALumnos_Ciclos {
          
          
          
-          int pregunta = Integer.parseInt(JOptionPane.showInputDialog("¿De quien quieres saber la calificacion?"));
+          int pregunta = Integer.parseInt(JOptionPane.showInputDialog("¿De quien quieres saber la calificacion? \n" + 
+                  "1.- Jose. \n" + 
+                  "2.- Karla. \n" + 
+                  "3.-Christian." ));
           //Carlos
           if(pregunta == 1){
-               int VectorFisica[] = {3,10,6,9,7};
-         int VectorHistoria[] = {3,10,6,9,7};
-         int VectorMatematicas[] = {3,10,6,9,7};
+               int VectorFisica[] = {1,10,6,9,7};
+               int VectorHistoria[] = {3,10,6,9,7};
+               int VectorMatematicas[] = {3,10,6,9,7};
               String opcion = "0";
-              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "\n" + VectorFisica.length + "\n" + VectorHistoria.length + "\n" + VectorMatematicas.length);
+              
+              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "Unidades de fisica: \n" + VectorFisica.length + "\n" + "Unidades de historia: \n" + VectorHistoria.length + "\n" + "Unidades de matematicas: \n" + VectorMatematicas.length);
               do{
                   
-               opcion =JOptionPane.showInputDialog("¿Quieres ver sus calificaciones y promedio final?");
+               opcion =JOptionPane.showInputDialog("¿Quieres ver su promedio final en general por las 3 materias?");
                if(opcion.equals("si")){     
           for (int i = 0; i < VectorFisica.length; i++) {
-              int suma1 = 0;
+              
+                  int suma1 = 0;
                   int suma2 = 0;
                   int suma3 = 0;
                   int SumaVectores = 0;
-              suma1 = suma1 + VectorFisica[i]/5;
-              suma2 = suma2 + VectorMatematicas[i]/5;
-              suma3 = suma3 + VectorHistoria[i]/5;
+              suma1 = suma1 + VectorFisica[i];
+              suma2 = suma2 + VectorMatematicas[i];
+              suma3 = suma3 + VectorHistoria[i];
               
-              SumaVectores = suma1 + suma2 + suma3/3;
+               SumaVectores = (suma1 + suma2 + suma3) / 3;
               if(SumaVectores >= 7){
-                  System.out.println("Aprobó. Aqui estan sus resultados: \n" + SumaVectores);
+                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
               }else{
-                  System.out.println("Reprobó. Aquí está su resultado: \n" + SumaVectores);
+                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
               }
                   
               }        
@@ -52,28 +57,31 @@ public class ALumnos_Ciclos {
               //Karla
               }else if(pregunta == 2){
                   
-                  int VectorFisica[] = {3,10,6,9,7};
-         int VectorHistoria[] = {3,10,6,9,7};
-         int VectorMatematicas[] = {3,10,6,9,7};
+                   int VectorFisica[] = {1,10,6,9,7};
+               int VectorHistoria[] = {3,10,6,9,7};
+               int VectorMatematicas[] = {3,10,6,9,7};
               String opcion = "0";
+              
               System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "\n" + VectorFisica.length + "\n" + VectorHistoria.length + "\n" + VectorMatematicas.length);
               do{
                   
                opcion =JOptionPane.showInputDialog("¿Quieres ver sus calificaciones y promedio final?");
                if(opcion.equals("si")){     
           for (int i = 0; i < VectorFisica.length; i++) {
-              int suma = 0;
+              
+                  int suma1 = 0;
                   int suma2 = 0;
                   int suma3 = 0;
-              suma = suma + VectorFisica[i]/5;
-              suma2 = suma2 + VectorMatematicas[i]/5;
-              suma3 = suma3 + VectorHistoria[i]/5;
-              int SumaVectores = 0;
-              SumaVectores = (suma + suma2 + suma3)/3;
+                  int SumaVectores = 0;
+              suma1 = suma1 + VectorFisica[i];
+              suma2 = suma2 + VectorMatematicas[i];
+              suma3 = suma3 + VectorHistoria[i];
+              
+               SumaVectores = (suma1 + suma2 + suma3) / 3;
               if(SumaVectores >= 7){
-                  System.out.println("Aprobó. Aqui estan sus resultados: \n" + SumaVectores);
+                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
               }else{
-                  System.out.println("Reprobó. Aquí está su resultado: \n" + suma);
+                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
               }
                   
               }        
@@ -81,7 +89,40 @@ public class ALumnos_Ciclos {
                   System.out.println("Bueno");
               }
               }while(opcion.equals("si"));
-              //Karla
+              //Christian
+              }else if(pregunta == 3){
+                   int VectorFisica[] = {1,10,6,9,7};
+               int VectorHistoria[] = {3,10,6,9,7};
+               int VectorMatematicas[] = {3,10,6,9,7};
+              String opcion = "0";
+              
+              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "\n" + VectorFisica.length + "\n" + VectorHistoria.length + "\n" + VectorMatematicas.length);
+              do{
+                  
+               opcion =JOptionPane.showInputDialog("¿Quieres ver sus calificaciones y promedio final?");
+               if(opcion.equals("si")){     
+          for (int i = 0; i < VectorFisica.length; i++) {
+              
+                  int suma1 = 0;
+                  int suma2 = 0;
+                  int suma3 = 0;
+                  int SumaVectores = 0;
+              suma1 = suma1 + VectorFisica[i];
+              suma2 = suma2 + VectorMatematicas[i];
+              suma3 = suma3 + VectorHistoria[i];
+              
+               SumaVectores = (suma1 + suma2 + suma3) / 3;
+              if(SumaVectores >= 7){
+                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
+              }else{
+                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
+              }
+                  
+              }        
+          }else{
+                  System.out.println("Bueno");
+              }
+              }while(opcion.equals("si"));
               }
                   
               }
