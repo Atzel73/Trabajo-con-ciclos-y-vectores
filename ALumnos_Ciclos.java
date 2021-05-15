1bfffb5d15adf376e97b32dad5f6b075e950b73a
Atzel73 <axelandrade638mail.com>
2021-05-15 18:46:05 -0500

    Re trabajo editado con ciclos terminado.

--- ALumnos_Ciclos.java
+++ ALumnos_Ciclos.java
@@ -20,99 +20,93 @@ public class ALumnos_Ciclos {
         int result = 0;
         String pregunta = "0";
         String Alumno = "0";
-       Alumno = JOptionPane.showInputDialog("¿De quien quieres saber la calificacion?\n" + "1.-Jose \n" + "2.-Karla \n" + "3.-Christian");
-do{
-      
-       //Jose
-       if(Alumno.equals("1")){
-           
-            System.out.println("Has elegido a Jose\n.");
-            
-            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
-            
-            if(pregunta.equals("si")){
-                int i = 0;
-                
-             
-                for ( i = 0; i <= Calificacion.length; i++) {
-                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
-                    cont = cont + Calificacion[i];
-                    
-                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
-                 
-                      result = cont / 5;
-                if(result >= 7){
-                   System.out.println("Aprobó." + result);
-               }else{
-                   System.out.println("Reprobó." + result);
-               }
-                 
+        Alumno = JOptionPane.showInputDialog("¿De quien quieres saber la calificacion?\n" + "1.-Jose \n" + "2.-Karla \n" + "3.-Christian");
+        do {
 
-            }
-       }else{
-                System.out.println("Bueno");
-            }
-            //Karla
-    }else  if(Alumno.equals("2")){
-           
-            System.out.println("Has elegido a Karla \n.");
-            
-            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
-            
-            if(pregunta.equals("si")){
-                int i = 0;
-                
-             
-                for ( i = 0; i <= Calificacion.length; i++) {
-                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
-                    cont = cont + Calificacion[i];
-                    
-                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
-                 
-                      result = cont / 5;
-                if(result >= 7){
-                   System.out.println("Aprobó." + result);
-               }else{
-                   System.out.println("Reprobó." + result);
-               }
-                 
+            //Jose
+            if (Alumno.equals("1")) {
 
-            }
-       }else{
-                System.out.println("Bueno");
-            }
-       
-    }else if(Alumno.equals("3")){
-           //Christian
-            System.out.println("Has elegido a Christian \n.");
-            
-            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
-            
-            if(pregunta.equals("si")){
-                int i = 0;
-                
-             
-                for ( i = 0; i <= Calificacion.length; i++) {
-                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
-                    cont = cont + Calificacion[i];
-                    
-                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
-                 
-                      result = cont / 5;
-                if(result >= 7){
-                   System.out.println("Aprobó." + result);
-               }else{
-                   System.out.println("Reprobó." + result);
-               }
-                 
+                System.out.println("Has elegido a Jose\n.");
 
+                pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+
+                if (pregunta.equals("si")) {
+                    int i = 0;
+
+                    for (i = 0; i <= Calificacion.length; i++) {
+                        Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                        cont = cont + Calificacion[i];
+
+                        System.out.println("Aqui esta la calificacion: " + Calificacion[i]);
+
+                        result = cont / 5;
+                        if (result >= 7) {
+                            System.out.println("Aprobó." + result);
+                        } else {
+                            System.out.println("Reprobó." + result);
+                        }
+
+                    }
+                } else {
+                    System.out.println("Bueno");
+                }
+                //Karla
+            } else if (Alumno.equals("2")) {
+
+                System.out.println("Has elegido a Karla \n.");
+
+                pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+
+                if (pregunta.equals("si")) {
+                    int i = 0;
+
+                    for (i = 0; i <= Calificacion.length; i++) {
+                        Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                        cont = cont + Calificacion[i];
+
+                        System.out.println("Aqui esta la calificacion: " + Calificacion[i]);
+
+                        result = cont / 5;
+                        if (result >= 7) {
+                            System.out.println("Aprobó." + result);
+                        } else {
+                            System.out.println("Reprobó." + result);
+                        }
+
+                    }
+                } else {
+                    System.out.println("Bueno");
+                }
+
+            } else if (Alumno.equals("3")) {
+                //Christian
+                System.out.println("Has elegido a Christian \n.");
+
+                pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+
+                if (pregunta.equals("si")) {
+                    int i = 0;
+
+                    for (i = 0; i <= Calificacion.length; i++) {
+                        Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                        cont = cont + Calificacion[i];
+
+                        System.out.println("Aqui esta la calificacion: " + Calificacion[i]);
+
+                        result = cont / 5;
+                        if (result >= 7) {
+                            System.out.println("Aprobó." + result);
+                        } else {
+                            System.out.println("Reprobó." + result);
+                        }
+
+                    }
+                } else {
+                    System.out.println("Bueno");
+                }
             }
-       }else{
-                System.out.println("Bueno");
-            }
-    }
-}while (Alumno == "");
+        } while (Alumno == "");
 
         System.out.println("No has elegido a nadie. Vuelve a elegir");
-}
+    }
 }
