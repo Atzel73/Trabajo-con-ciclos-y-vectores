8e7a86f9ebef651dd0499f3240df8d649d292f18
Atzel73 <axelandrade638mail.com>
2021-05-15 15:50:31 -0500

    Re trabajo editado con ciclos terminado.

--- ALumnos_Ciclos.java
+++ ALumnos_Ciclos.java
@@ -12,119 +12,107 @@ import javax.swing.JOptionPane;
  * @author Propietario
  */
 public class ALumnos_Ciclos {
-      public static void main (String[] args){
-         
-         
-         
-          int pregunta = Integer.parseInt(JOptionPane.showInputDialog("¿De quien quieres saber la calificacion? \n" + 
-                  "1.- Jose. \n" + 
-                  "2.- Karla. \n" + 
-                  "3.-Christian." ));
-          //Carlos
-          if(pregunta == 1){
-               int VectorFisica[] = {1,10,6,9,7};
-               int VectorHistoria[] = {3,10,6,9,7};
-               int VectorMatematicas[] = {3,10,6,9,7};
-              String opcion = "0";
-              
-              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "Unidades de fisica: \n" + VectorFisica.length + "\n" + "Unidades de historia: \n" + VectorHistoria.length + "\n" + "Unidades de matematicas: \n" + VectorMatematicas.length);
-              do{
-                  
-               opcion =JOptionPane.showInputDialog("¿Quieres ver su promedio final en general por las 3 materias?");
-               if(opcion.equals("si")){     
-          for (int i = 0; i < VectorFisica.length; i++) {
-              
-                  int suma1 = 0;
-                  int suma2 = 0;
-                  int suma3 = 0;
-                  int SumaVectores = 0;
-              suma1 = suma1 + VectorFisica[i];
-              suma2 = suma2 + VectorMatematicas[i];
-              suma3 = suma3 + VectorHistoria[i];
-              
-               SumaVectores = (suma1 + suma2 + suma3) / 3;
-              if(SumaVectores >= 7){
-                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
-              }else{
-                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
-              }
-                  
-              }        
-          }else{
-                  System.out.println("Bueno");
-              }
-              }while(opcion.equals("si"));
-              //Karla
-              }else if(pregunta == 2){
-                  
-                   int VectorFisica[] = {1,10,6,9,7};
-               int VectorHistoria[] = {3,10,6,9,7};
-               int VectorMatematicas[] = {3,10,6,9,7};
-              String opcion = "0";
-              
-              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "\n" + VectorFisica.length + "\n" + VectorHistoria.length + "\n" + VectorMatematicas.length);
-              do{
-                  
-               opcion =JOptionPane.showInputDialog("¿Quieres ver sus calificaciones y promedio final?");
-               if(opcion.equals("si")){     
-          for (int i = 0; i < VectorFisica.length; i++) {
-              
-                  int suma1 = 0;
-                  int suma2 = 0;
-                  int suma3 = 0;
-                  int SumaVectores = 0;
-              suma1 = suma1 + VectorFisica[i];
-              suma2 = suma2 + VectorMatematicas[i];
-              suma3 = suma3 + VectorHistoria[i];
-              
-               SumaVectores = (suma1 + suma2 + suma3) / 3;
-              if(SumaVectores >= 7){
-                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
-              }else{
-                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
-              }
-                  
-              }        
-          }else{
-                  System.out.println("Bueno");
-              }
-              }while(opcion.equals("si"));
-              //Christian
-              }else if(pregunta == 3){
-                   int VectorFisica[] = {1,10,6,9,7};
-               int VectorHistoria[] = {3,10,6,9,7};
-               int VectorMatematicas[] = {3,10,6,9,7};
-              String opcion = "0";
-              
-              System.out.println("Aqui estan las unidades de las materias de Carlos: \n" + "\n" + VectorFisica.length + "\n" + VectorHistoria.length + "\n" + VectorMatematicas.length);
-              do{
-                  
-               opcion =JOptionPane.showInputDialog("¿Quieres ver sus calificaciones y promedio final?");
-               if(opcion.equals("si")){     
-          for (int i = 0; i < VectorFisica.length; i++) {
-              
-                  int suma1 = 0;
-                  int suma2 = 0;
-                  int suma3 = 0;
-                  int SumaVectores = 0;
-              suma1 = suma1 + VectorFisica[i];
-              suma2 = suma2 + VectorMatematicas[i];
-              suma3 = suma3 + VectorHistoria[i];
-              
-               SumaVectores = (suma1 + suma2 + suma3) / 3;
-              if(SumaVectores >= 7){
-                  System.out.println("Aqui está su promedio final .Aprobó: \n" + SumaVectores);
-              }else{
-                  System.out.println("Aqui está su promedio final .Reprobó: \n" + SumaVectores);
-              }
-                  
-              }        
-          }else{
-                  System.out.println("Bueno");
-              }
-              }while(opcion.equals("si"));
-              }
-                  
-              }
-      }
 
+    public static void main(String[] args) {
+        int Longitud = 0;
+        int cont = 0;
+        int Calificacion[] = new int[5];
+        int result = 0;
+        String pregunta = "0";
+        String Alumno = "0";
+       Alumno = JOptionPane.showInputDialog("¿De quien quieres saber la calificacion?\n" + "1.-Jose \n" + "2.-Karla \n" + "3.-Christian");
+do{
+      
+       //Jose
+       if(Alumno.equals("1")){
+           
+            System.out.println("Has elegido a Jose\n.");
+            
+            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+            
+            if(pregunta.equals("si")){
+                int i = 0;
+                
+             
+                for ( i = 0; i <= Calificacion.length; i++) {
+                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                    cont = cont + Calificacion[i];
+                    
+                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
+                 
+                      result = cont / 5;
+                if(result >= 7){
+                   System.out.println("Aprobó." + result);
+               }else{
+                   System.out.println("Reprobó." + result);
+               }
+                 
+
+            }
+       }else{
+                System.out.println("Bueno");
+            }
+            //Karla
+    }else  if(Alumno.equals("2")){
+           
+            System.out.println("Has elegido a Karla \n.");
+            
+            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+            
+            if(pregunta.equals("si")){
+                int i = 0;
+                
+             
+                for ( i = 0; i <= Calificacion.length; i++) {
+                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                    cont = cont + Calificacion[i];
+                    
+                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
+                 
+                      result = cont / 5;
+                if(result >= 7){
+                   System.out.println("Aprobó." + result);
+               }else{
+                   System.out.println("Reprobó." + result);
+               }
+                 
+
+            }
+       }else{
+                System.out.println("Bueno");
+            }
+       
+    }else if(Alumno.equals("3")){
+           //Christian
+            System.out.println("Has elegido a Christian \n.");
+            
+            pregunta = JOptionPane.showInputDialog("¿Quieres introducir sus calificaciones?");
+            
+            if(pregunta.equals("si")){
+                int i = 0;
+                
+             
+                for ( i = 0; i <= Calificacion.length; i++) {
+                    Calificacion[i] = Integer.parseInt(JOptionPane.showInputDialog("Ingresa las calificaciones mayores a 5: "));
+                    cont = cont + Calificacion[i];
+                    
+                 System.out.println("Aqui esta la calificacion: " + Calificacion[i]);   
+                 
+                      result = cont / 5;
+                if(result >= 7){
+                   System.out.println("Aprobó." + result);
+               }else{
+                   System.out.println("Reprobó." + result);
+               }
+                 
+
+            }
+       }else{
+                System.out.println("Bueno");
+            }
+    }
+}while (Alumno == "");
+
+        System.out.println("No has elegido a nadie. Vuelve a elegir");
+}
+}
